ce0c7e681b77a762ab194af9aa44e6134f9dd7cf
MMS <mm.silva@outlook.com.br>
2016-05-04 14:56:02 -0300

    adição do botão salvar na tela de nova  consulta

--- app/src/main/java/bestsolutions/net/consultamais/CadastrarConsulta.java
+++ app/src/main/java/bestsolutions/net/consultamais/CadastrarConsulta.java
@@ -4,8 +4,34 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentTransaction;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.text.CollationElementIterator;
+import java.util.Calendar;
+
+import android.text.format.DateFormat;
+import android.view.View;
+
+import java.util.Calendar;
 
 public class CadastrarConsulta extends AppCompatActivity {
+
+    private int Ano;
+    private int Mes;
+    private int Dia;
+    private int Hora;
+    private int Minuto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +49,49 @@ public class CadastrarConsulta extends AppCompatActivity {
         if (ab != null) {
             ab.setDisplayHomeAsUpEnabled(true);
         }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_consulta);
+        InicializaListeners();
+
+        final Calendar cal = Calendar.getInstance();
+        Ano = cal.get(Calendar.YEAR);
+        Mes = cal.get(Calendar.MONTH);
+        Dia = cal.get(Calendar.DAY_OF_MONTH);
+        Hora = cal.get(Calendar.HOUR);
+        Minuto = cal.get(Calendar.MINUTE);
+
+        AtualizarData();
+        AtualizarHora();
+
+    private void AtualizarData() {
+        CollationElementIterator txtData = null;
+        txtData.setText(String.valueOf(new StringBuilder().append(Dia).append("/").append(Mes + 1).append("/").append(Ano).append(" ")));
+    }
+
+    private void AtualizarHora() {
+        CollationElementIterator txtHora = null;
+        txtHora.setText(String.valueOf(new StringBuilder().append(Hora).append(":").append(Minuto)));
+    }
+
+
+    public void InicializaListeners() {
+        TextView txtData = (TextView) findViewById(R.id.txtData);
+        TextView txtHora = (TextView) findViewById(R.id.txtHora);
+    }
+
+    public Dialog onCreateDialog(Bundle savedInstanceState)
+    {
+
+        final Calendar c = Calendar.getInstance();
+        Hora = c.get(Calendar.HOUR_OF_DAY);
+        Minuto = c.get(Calendar.MINUTE);
+        return new TimePickerDialog(
+                getApplication(),
+                this,
+                Hora,
+                Minuto,
+                DateFormat.is24HourFormat(getApplication());
+    }
         /*Toolbar actionbar = (Toolbar) findViewById(R.id.actionbar);
         if (null != actionbar) {
             actionbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
@@ -39,5 +108,6 @@ public class CadastrarConsulta extends AppCompatActivity {
             actionbar.inflateMenu(R.menu.settings);
         }
 */
-    }
+}
+
 }
